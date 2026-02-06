db9bfcb6b1b7976361e383bb772d0980ea83b6bc
Tu Nombre <santiagomunoz8793@gmail.com>
2026-02-06 16:52:23 -0600

    Entrega03 maquina enigma

--- src/main.c
+++ src/main.c
@@ -1,7 +1,38 @@
 #include <stdio.h>
 
-int main()
-{
-    printf("Hello World!\n");
+int ShiftLeftCircular(int value) {
+    return ((value << 1) | (value >> 7)) & 0xFF;
+}
+
+int InvertBits(int value) {
+    return (~value) & 0xFF;
+}
+
+int ShiftRightCircular(int value) {
+    return ((value >> 1) | (value << 7)) & 0xFF;
+}
+
+int ApplyMask(int value) {
+    return value ^ 129;
+}
+
+int EncryptValue(int value) {
+    value = ShiftLeftCircular(value);
+    value = InvertBits(value);
+    value = ShiftRightCircular(value);
+    value = ApplyMask(value);
+    return value;
+}
+
+int main() {
+    char inputChar;
+    
+    printf("Ingrese el caracter a encriptar: ");
+    scanf(" %c", &inputChar);
+    
+    int result = EncryptValue((int)inputChar);
+    
+    printf("%c -> %c\n", inputChar, (char)result);
+    
     return 0;
 }
